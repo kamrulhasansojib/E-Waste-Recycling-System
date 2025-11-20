b1c78e1228d01a71fc3972fd10ccbfbea8a697f8
kamrulhasansojib <kamrulhasansojib19@gmail.com>
2025-11-21 00:55:38 +0600

    Create Database connection.php file

---
+++ database/connection.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "localhost";
+$db_name = "e_trieve";
+$username = "root";
+$password = ""; 
+
+$conn = new mysqli($host, $username, $password, $db_name);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
